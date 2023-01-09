9bd931a152e09cdc8b879deec29869b3869592d9
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2023-01-09 12:42:22 +0100

    First Commit

---
+++ react/src/contexts/ContextProvider.jsx
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+
+const StateContext = createContext({
+    user: null,
+    token: null,
+    notification: null,
+    setUser: () => {},
+    setToken: () => { },
+    setNotification: () => {},
+});
+
+export const ContextProvider = ({ children }) => {
+    const [user, setUser] = useState({});
+    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+    const [notification, _setNotification] = useState('');
+
+    const setNotification = (message) => {
+        _setNotification(message);
+        setTimeout(() => {
+            _setNotification('');
+        },5000);
+    }
+
+    const setToken = (token) => {
+        _setToken(token);
+        if (token) {
+            localStorage.setItem('ACCESS_TOKEN', token);
+        } else {
+            localStorage.removeItem('ACCESS_TOKEN');
+        }
+    }
+
+    return (
+        <StateContext.Provider value={{
+            user,
+            token,
+            setUser,
+            setToken,
+            notification,
+            setNotification
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
